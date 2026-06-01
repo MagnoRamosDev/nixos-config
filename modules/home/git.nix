3895dcfaf49b7a53a39806c50e3fa881c77d3aa5
MagnoRamosDev <magnoramosdeveloper@gmail.com>
2026-06-01 17:59:11 -0300

    feat: adiciona regras de git e roteamento ssh para github e codeberg

---
+++ modules/home/git.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+
+{
+  # ==========================================
+  # CONFIGURAÇÕES GLOBAIS DO GIT
+  # ==========================================
+  programs.git = {
+    enable = true;
+    userName = "MagnoRamosDev";
+    userEmail = "magnoramosdeveloper@gmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = true;
+    };
+  };
+
+  # ==========================================
+  # ROTEAMENTO SSH (Múltiplas Plataformas)
+  # ==========================================
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+        identitiesOnly = true;
+      };
+
+      "codeberg.org" = {
+        hostname = "codeberg.org";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519"; # Usando a mesma chave
+        identitiesOnly = true;
+      };
+    };
+  };
+
+  services.ssh-agent.enable = true;
+}
