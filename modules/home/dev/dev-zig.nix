fc1530cf8f9918c06b45ea6cc5de0b18b2151c2b
MagnoRamosDev <magnoramosdeveloper@gmail.com>
2026-06-03 13:35:59 -0300

    update: nix to 26.05

--- modules/home/dev/dev-zig.nix
+++ modules/home/dev/dev-zig.nix
@@ -2,7 +2,7 @@
 
 {
   home.packages = with pkgs; [
-    zig         # Compilador oficial
-    zls         # Zig Language Server
+    zig # Compilador oficial
+    zls # Zig Language Server
   ];
 }
