642078dcc913203711f372b3d38322ace7e118bc
MagnoRamosDev <magnoramosdeveloper@gmail.com>
2026-06-11 15:48:01 -0300

    added: python3

--- modules/home/dev/dev-python.nix
+++ modules/home/dev/dev-python.nix
@@ -2,11 +2,13 @@
 
 {
   home.packages = with pkgs; [
-    python3
+    python313
     virtualenv
     pyright
     # python3Packages.python-lsp-server # Outra alternativa popular, caso não goste do pyright
     ruff
     # black # Descomente se preferir usar o formatador padrão clássico da comunidade
+    stdenv.cc.cc.lib
+    zlib
   ];
 }
