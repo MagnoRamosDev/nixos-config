fb8da07ab962e991dca051a5faa2803143fc5cad
MagnoRamosDev <magnoramosdeveloper@gmail.com>
2026-06-12 21:59:57 -0300

    added: foundryvtt

--- hosts/magno-pc/configuration.nix
+++ hosts/magno-pc/configuration.nix
@@ -127,6 +127,25 @@
     config.common.default = "wlr";
   };
 
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.foundryvtt = {
+      image = "felddy/foundryvtt:release";
+      ports = [ "30000:30000" ];
+      volumes = [
+        "/var/lib/foundryvtt:/data"
+      ];
+      environmentFiles = [
+        "/mnt/armazenamento/FoundryVTT/foundry.env"
+      ];
+    };
+  };
+
   # ==========================================
   # PROGRAMAS GLOBAIS E PACOTES
   # ==========================================
