6b6213850271f64bf86dc0bb285a7ac616b7ae72
MagnoRamosDev <magnoramosdeveloper@gmail.com>
2026-06-12 21:55:45 -0300

    added: foundryvtt

--- modules/home/virtualisation.nix
+++ modules/home/virtualisation.nix
@@ -1,6 +1,12 @@
 { ... }:
 
 {
+  virtualisation.podman = {
+    enable = true;
+
+    dockerCompat = true;
+  };
+
   virtualisation.oci-containers = {
     backend = "podman";
     containers.foundryvtt = {
