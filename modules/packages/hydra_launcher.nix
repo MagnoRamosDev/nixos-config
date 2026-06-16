64a79cf58faa170c9d560a1fc062288ab16fbd52
MagnoRamosDev <magnoramosdeveloper@gmail.com>
2026-06-16 14:51:28 -0300

    added: hydra launcher

--- modules/packages/hydra_launcher.nix
+++ modules/packages/hydra_launcher.nix
@@ -6,7 +6,10 @@ pkgs.appimageTools.wrapType2 {
   pname = "hydra-launcher";
   version = "4.0.0";
 
-  src = /home/magno/Documentos/appimages/hydralauncher-4.0.0.AppImage;
+  src = pkgs.fetchurl {
+    url = "https://github.com/hydralauncher/hydra/releases/download/v4.0.0/hydralauncher-4.0.0.AppImage";
+    hash = "sha256-epFymwQExsPHxAT/QOpL3cOOsbUg1WdttFQ+DKcAGRw=";
+  };
 
   extraPkgs = pkgs: with pkgs; [ ];
 }
