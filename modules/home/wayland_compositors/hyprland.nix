18cbf0fd15495fe7566f034b7a12ef7ccea86440
MagnoRamosDev <magnoramosdeveloper@gmail.com>
2026-07-16 20:52:39 -0300

    Remove Hyprland user configuration from module

--- modules/home/wayland_compositors/hyprland.nix
+++ modules/home/wayland_compositors/hyprland.nix
@@ -2,25 +2,4 @@
 
 {
   programs.hyprland.enable = true;
-
-  home-manager.users.magno = {
-    wayland.windowManager.hyprland = {
-      enable = true;
-      systemd.enable = true;
-
-      settings = {
-        "$terminal" = "ghostty";
-        "$mainMod" = "SUPER";
-
-        bind = [
-          "$mainMod, Q, exec, $terminal"
-          "$mainMod, M, exit,"
-        ];
-
-        windowrulev2 = [
-          "float, class:.*"
-        ];
-      };
-    };
-  };
 }
